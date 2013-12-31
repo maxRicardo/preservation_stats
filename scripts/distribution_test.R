9b890354fa10c1ac2bdd1d43a030ae4eb158a0a9
maxRicardo <xaradrim@gmail.com>
2013-12-30 20:19:10 -0400

    implementation for graphical approach to distribution test.

--- scripts/distribution_test.R
+++ scripts/distribution_test.R
@@ -1,7 +1,7 @@
 #Return a data frame with the summary of vlaues 
 #on a specific data set in a Frame
 
-Summary_info = function(data){
+summary_info = function(data){
   
   df = data.frame(mean = mean(data),
                   sd = sd(data),
@@ -12,12 +12,12 @@ Summary_info = function(data){
   
 }
 
-Distribution_test = function(data){
+distribution_test = function(data){
   
-  summ = Summary_info(data)
+  summ = summary_info(data)
   
   hist(data,probability=T)
-  #curve(dnorm(x,summ$mean,summ$sd),col = "red",)
-  
-  
+  curve(dnorm(x,summ$mean,summ$sd),-3*summ$mean,3*summ$mean,col = "red",add = T)
+  curve(dexp(x,1/summ$mean),-3*summ$mean,3*summ$mean,col = "blue",add = T)
+  curve(dunif(x,summ$min,summ$max),-3*summ$mean,3*summ$mean,col = "green",add = T) 
 }
